11a240015f3fc1e3f48dd3f13b6f8576807da36a
alexandra <alexandrabalean92@gmail.com>
2019-11-29 20:08:34 +0200

    Auth Section 3

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -1,5 +1,5 @@
-using DatingApp.API.Controllers.Models;
 using Microsoft.EntityFrameworkCore;
+using DatingApp.API.Models;
 
 namespace DatingApp.API.Data
 {
@@ -11,5 +11,6 @@ namespace DatingApp.API.Data
         }
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
